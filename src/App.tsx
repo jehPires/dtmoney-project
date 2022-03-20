58dea41584ffd6c39a4fa485d767f8a7e00e6e86
Jessica Pires <pires.jessik@gmail.com>
2022-03-19 21:27:47 -0300

    project finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import styled from 'styled-components';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import Modal from 'react-modal';
 
-function App() {
+import { GlobalStyle } from "./styles/global";
+import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import {TransactionsProvider} from "./hooks/useTransactions";
+
+Modal.setAppElement('#root'); //acessibilidade
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false); //incia como false,
+    //pois o modal está fechado
+
+    function handleOpenNewTransactionModal(){ //pattern-'handle' quando vou criar uma função que é
+        //executada a partir de uma ação do user
+        setIsNewTransactionModalOpen(true);
+    }
+    
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TransactionsProvider> 
+      <h1></h1>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      
+      <Dashboard />
+
+      <NewTransactionModal
+      isOpen ={isNewTransactionModalOpen} 
+      onRequestClose={handleCloseNewTransactionModal}
+      />
+     
+      <GlobalStyle />
+    </TransactionsProvider>
   );
 }
 
-export default App;
